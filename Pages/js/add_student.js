e63ca42c581084d3b02244446cf9d5f3ef0b4d9a
Ismail Magdy <92678464+ismailmagdyismail@users.noreply.github.com>
2023-04-15 23:36:13 +0200

    modifed js for add student , added verifification on blur + refactoring

--- Pages/js/add_student.js
+++ Pages/js/add_student.js
@@ -5,114 +5,129 @@ let levels = [1,2,3,4];
 let statuses = ["Enrolled in","Exiting","Re-entering"];
 
 /**
- * Input boxes
+ *
+ * BOX ID => BOX itself
  */
-let firstNameBox = document.querySelector('#first');
-let lastNameBox = document.querySelector('#last');
-let gpaBox = document.querySelector('#gpa');
-let idBox = document.querySelector('#id');
-let phoneNumberBox = document.querySelector('#phone');
-let emailBox = document.querySelector('#email');
+let boxes = {};
 
-let genderBox = document.querySelector('#gender');
-let dateOfBirthBox = document.querySelector('#date');
-let departmentBox = document.querySelector('#department');
-let levelBox = document.querySelector('#level');
-let statusBox = document.querySelector('#status');
+/**
+ * BoxID => Verification function
+ */
+let verifications = {};
+
+getInputBoxes();
+setUpBoxesVerification();
+setupBoxesActions();
 
 
 
+function getInputBoxes(){
+    let inputBoxes = document.querySelectorAll('input');
+    let selectBoxes = document.querySelectorAll('select');
+    for (let i = 0;i < inputBoxes.length;i++){
+        boxes[inputBoxes[i].id] = inputBoxes[i];
+    }
+    for (let i = 0;i < selectBoxes.length;i++){
+        boxes[selectBoxes[i].id] = selectBoxes[i];
+    }
+}
+function setUpBoxesVerification(){
+   verifications["first"] = isValidFirstName;
+   verifications["last"] = isValidLastName;
+   verifications["email"] = isValidEmail;
+   verifications["phone"] = isValidPhoneNumber;
+   verifications["gpa"] = isValidGPA;
+   verifications["id"] = isValidID;
+   verifications["gender"] = isValidGender;
+   verifications["department"] = isValidDepartment;
+   verifications["level"] = isValidLevel;
+   verifications["status"] = isValidStatus;
+}
+function setupBoxesActions(){
+    for (let boxID in boxes){
+        let box = boxes[boxID];
+        if(verifications[boxID] === undefined)
+            continue;
+        box.onblur = ()=>{
+            let isValid = verifications[box.id]();
+            if(!isValid){
+                displayError(box);
+            }
+            else{
+                removeError(box);
+            }
+        }
+    }
+}
 function displayError(inputBox){
     let errorBox = document.querySelector(`#${inputBox.id}-error`);
-    console.log(errorBox);
     errorBox.classList.remove("hide");
+}
+function removeError(inputBox){
+    let errorBox = document.querySelector(`#${inputBox.id}-error`);
+    errorBox.classList.add("hide");
 }
 
 /**
  * Validations for Student input
  */
-function isValidName(name){
+function isValidFirstName(){
+    let firstName = boxes["first"].value;
     const regex = /^[a-zA-Z]+$/;
-    return regex.test(name) && name.length > 1;
+    return regex.test(firstName) && firstName.length > 1;
 }
-
-function isValidGPA(gpa){
+function isValidLastName(){
+    let lastName = boxes["last"].value;
+    const regex = /^[a-zA-Z]+$/;
+    return regex.test(lastName) && lastName.length > 1;
+}
+function isValidGPA(){
+    let gpa = boxes["gpa"].value;
     return gpa > 0.0 && gpa < 4.0;
 }
 
-function isValidEmail(email){
+function isValidEmail(){
+    let email = boxes["email"].value;
     const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,}$/;
     return regex.test(email);
 }
 
-function isValidPhoneNumber(phoneNumber){
+function isValidPhoneNumber(){
+    let phoneNumber = boxes["phone"].value;
     const regex = /^(01)[0-9]{9}$/;
     return regex.test(phoneNumber);
 }
 
-function isValidID(id){
-    const regex = /^d{6,}$/;
-    return regex.test(id);
+function isValidID(){
+    let id = boxes["id"].value;
+    const regex = /^[0-9]+$/;
+    return regex.test(id) && id.length > 6;
 }
 
-function isValidGender(gender){
-    return genders.includes(gender);
+function isValidGender(){
+    return boxes["gender"].selectedIndex !== 0 ;
 }
-function isValidDepartment(department){
-    return departments.includes(department);
-}
-
-function isValidLevel(level){
-    return levels.includes(level);
+function isValidDepartment(){
+    return boxes["department"].selectedIndex !== 0 ;
 }
 
-function isValidStatus(status){
-    return statuses.includes(status);
+function isValidLevel(){
+    return boxes["level"].selectedIndex !== 0 ;
+}
+
+function isValidStatus(){
+    return boxes["status"].selectedIndex !== 0 ;
 }
 
 /**
  * Validation on Submitting student form
  */
 form.addEventListener('submit',(event)=>{
-    if(!isValidName(firstNameBox.value)){
-        displayError(firstNameBox);
-        event.preventDefault();
-    }
-    if(!isValidName(lastNameBox.value)){
-        displayError(lastNameBox);
-        event.preventDefault();
-    }
-    if(!isValidGPA(gpaBox.value)){
-        displayError(gpaBox);
-        event.preventDefault();
-    }
-    if(!isValidPhoneNumber(phoneNumberBox.value)){
-        displayError(phoneNumberBox);
-        event.preventDefault();
-        event.preventDefault();
-    }
-    if(!isValidID(idBox.value)){
-        displayError(idBox);
-        event.preventDefault();
-    }
-    if(!isValidEmail(emailBox.value)){
-        displayError(emailBox);
-        event.preventDefault();
-    }
-    if(!isValidGender(genderBox.value)){
-        displayError(genderBox);
-        event.preventDefault();
-    }
-    if(!isValidDepartment(departmentBox.value)){
-        displayError(departmentBox);
-        event.preventDefault();
-    }
-    if(!isValidLevel(levelBox.value)){
-        displayError(levelBox);
-        event.preventDefault();
-    }
-    if(!isValidStatus(statusBox.status)){
-        displayError(statusBox);
-        event.preventDefault();
+    for (let boxID in verifications){
+        let isValid = verifications[boxID]();
+        if(!isValid){
+            displayError(boxes[boxID]);
+            event.preventDefault();
+        }
     }
 })
