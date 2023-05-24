12ea4366a01846d4a0a824d58ccbdd720b859228
ismail <ismailmagdy88@gmail.com>
2023-05-24 19:38:48 +0300

    Linked Pages together , added urls

--- BusinessLogic/urls.py
+++ BusinessLogic/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
-
+    path('', views.homePage, name = 'homePage'),
+    path('home/', views.homePage, name = 'homePage'),
+    path('registration/', views.registration, name = 'registration'),
+    path('departmentAssignment/', views.departmentAssignment, name = 'departmentAssignment'),
+    path('updateData/', views.updateData, name = 'updateData'),
+    path('studentsList/', views.studentsList, name = 'studentsList'),
+    path('info/', views.info, name = 'info'),
 ]
--- BusinessLogic/views.py
+++ BusinessLogic/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def homePage(request):
+    if request.method == 'GET':
+        return render(request, 'main_menu.html')
+
+
+def registration(request):
+    if request.method == 'GET':
+        return render(request, 'add_student.html')
+
+
+def departmentAssignment(request):
+    if request.method == 'GET':
+        return render(request, 'assign_department.html')
+
+
+def updateData(request):
+    if request.method == 'GET':
+        return render(request, 'Update.html')
+
+
+def studentsList(request):
+    if request.method == 'GET':
+        return render(request, 'Student_list.html')
+
+
+def info(request):
+    if request.method == 'GET':
+        return render(request, 'info.html')
+
+
