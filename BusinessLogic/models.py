d68997ffe93c7ad218a578d4a95a230f3418570d
ismail <ismailmagdy88@gmail.com>
2023-05-24 20:11:37 +0300

    Added Student Model , Registered it

--- BusinessLogic/models.py
+++ BusinessLogic/models.py
@@ -4,8 +4,14 @@ from datetime import datetime
 
 class Student(models.Model):
     id = models.IntegerField(primary_key=True)
-    firstName = models.CharField(max_length=20, default="Not specified")
-    lastName = models.CharField(max_length=20, default="Not specified")
+    firstName = models.CharField(max_length=20, null=False)
+    lastName = models.CharField(max_length=20, null=False)
     dateOfBirth = models.DateField(default=datetime(2003, 1, 1))
     gpa = models.DecimalField(max_digits=3, decimal_places=2, default=2)
-
+    department = models.CharField(max_length=12, null=False)
+    level = models.IntegerField(null= False)
+    email = models.CharField(max_length=25, default="N/A", unique=True)
+    phone = models.CharField(max_length=12, null=False)
+    status = models.CharField(max_length=10, null=False)
+    def __str__(self):
+        return self.firstName+self.lastName+f" {str(self.id)}"
